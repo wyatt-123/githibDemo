a1584996820507b1e1ef060d659a2b25c98c6a4b
wyatt <f227006900@hmail.com>
2023-01-24 14:05:24 +0800

    Initial Commit

---
+++ githib/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  githib
+//
+//  Created by 林家麒 on 2023/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ githib/githibApp.swift
@@ -0,0 +1,17 @@
+//
+//  githibApp.swift
+//  githib
+//
+//  Created by 林家麒 on 2023/1/24.
+//
+
+import SwiftUI
+
+@main
+struct githibApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
